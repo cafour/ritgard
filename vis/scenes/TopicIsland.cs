ec683a1790309439dc1517c4f59167c05069ce2c
Adam Štěpánek <git@cafour.cz>
2025-10-01 22:11:41 +0200

    Move the leveled terrain heightmap calculation to another method

--- vis/scenes/TopicIsland.cs
+++ vis/scenes/TopicIsland.cs
@@ -47,23 +47,18 @@ public partial class TopicIsland : Node3D
         }
 
         var points = Topic.Ids.Select(id => Overlord.Instance.Positions.GetValueOrDefault(id)).ToArray();
-        var coords = points.Select(v => new Coordinate(v.X, v.Z)).ToArray();
-        var pointCloud = Geometry.DefaultFactory.CreateMultiPointFromCoords(coords);
-        var hull = ConcaveHull.ConcaveHullByLengthRatio(pointCloud, 0.3);
-        var kdTree = new KdTree<Issue>();
-        foreach (var id in Topic.Ids)
-        {
-            var pos = Overlord.Instance.Positions[id];
-            kdTree.Insert(new Coordinate(pos.X, pos.Z), Overlord.Instance.Data[id]);
-        }
-        var bbox = new Aabb(
-            new Vector3((float)hull.Envelope.Coordinates[0].X, 0f, (float)hull.Envelope.Coordinates[0].Y),
-            new Vector3(
-                (float)Math.Abs(hull.Envelope.Coordinates[0].X - hull.Envelope.Coordinates[3].X),
-                points.Max(p => p.Y) + 1,
-                (float)Math.Abs(hull.Envelope.Coordinates[0].Y - hull.Envelope.Coordinates[1].Y)
-            )
+
+        var startCorner = new Vector3(
+            points.Min(p => p.X),
+            0,
+            points.Min(p => p.Z)
         );
+        var endCorner = new Vector3(
+            points.Max(p => p.X),
+            points.Max(p => p.Y),
+            points.Max(p => p.Z)
+        );
+        var bbox = new Aabb(startCorner, endCorner - startCorner);
         var intSize = Utils.RoundToInt(bbox.Size) + new Vector3I(StructureRadius * 2, 0, StructureRadius * 2);
         var buffer = new StructureBuffer(
             size: intSize,
@@ -72,46 +67,7 @@ public partial class TopicIsland : Node3D
         );
 
         Heightmap = new byte[intSize.Z, intSize.X];
-        var testPoint = Geometry.DefaultFactory.CreatePoint(new Coordinate(0, 0));
-        for (int z = 0; z < intSize.Z; ++z)
-        {
-            for (int x = 0; x < intSize.X; ++x)
-            {
-                var px = x + bbox.Position.X;
-                var py = z + bbox.Position.Z;
-
-                testPoint.Coordinate.X = px;
-                testPoint.Coordinate.Y = py;
-
-                var nearestPoint = kdTree.NearestNeighbor(testPoint.Coordinate);
-                if (nearestPoint is null || nearestPoint.Count == 0)
-                {
-                    continue;
-                }
-
-                var nearestIssue = nearestPoint.Data;
-                var nearestPointHeight = Overlord.Instance.Positions[nearestIssue.Id].Y;
-
-                if (hull.Contains(testPoint))
-                {
-                    Heightmap[z, x] = (byte)Math.Clamp(Mathf.RoundToInt(nearestPointHeight), 0, 255);
-                }
-                else
-                {
-                    var distance = nearestPoint.Coordinate.Distance(testPoint.Coordinate);
-                    if (distance < StructureRadius)
-                    {
-                        Heightmap[z, x] = (byte)nearestPointHeight;
-                    }
-                    else
-                    {
-                        distance -= StructureRadius;
-                        var height = nearestPointHeight / (distance * distance + 1);
-                        Heightmap[z, x] = (byte)height;
-                    }
-                }
-            }
-        }
+        ComputeLeveledHeightmap(points, intSize, bbox);
 
         for (int y = 0; y < Heightmap.GetLength(0); ++y)
         {
@@ -164,6 +120,60 @@ public partial class TopicIsland : Node3D
         if (material is not null)
         {
             material.NextPass = IsHighlighted ? HighlightMaterial : null;
+        }
+    }
+
+    private void ComputeLeveledHeightmap(IEnumerable<Vector3> points, Vector3I intSize, Aabb bbox)
+    {
+        var coords = points.Select(v => new Coordinate(v.X, v.Z)).ToArray();
+        var pointCloud = Geometry.DefaultFactory.CreateMultiPointFromCoords(coords);
+        var hull = ConcaveHull.ConcaveHullByLengthRatio(pointCloud, 0.3);
+        var kdTree = new KdTree<Issue>();
+        foreach (var id in Topic.Ids)
+        {
+            var pos = Overlord.Instance.Positions[id];
+            kdTree.Insert(new Coordinate(pos.X, pos.Z), Overlord.Instance.Data[id]);
+        }
+        
+        var testPoint = Geometry.DefaultFactory.CreatePoint(new Coordinate(0, 0));
+        for (int z = 0; z < intSize.Z; ++z)
+        {
+            for (int x = 0; x < intSize.X; ++x)
+            {
+                var px = x + bbox.Position.X;
+                var py = z + bbox.Position.Z;
+
+                testPoint.Coordinate.X = px;
+                testPoint.Coordinate.Y = py;
+
+                var nearestPoint = kdTree.NearestNeighbor(testPoint.Coordinate);
+                if (nearestPoint is null || nearestPoint.Count == 0)
+                {
+                    continue;
+                }
+
+                var nearestIssue = nearestPoint.Data;
+                var nearestPointHeight = Overlord.Instance.Positions[nearestIssue.Id].Y;
+
+                if (hull.Contains(testPoint))
+                {
+                    Heightmap[z, x] = (byte)Math.Clamp(Mathf.RoundToInt(nearestPointHeight), 0, 255);
+                }
+                else
+                {
+                    var distance = nearestPoint.Coordinate.Distance(testPoint.Coordinate);
+                    if (distance < StructureRadius)
+                    {
+                        Heightmap[z, x] = (byte)nearestPointHeight;
+                    }
+                    else
+                    {
+                        distance -= StructureRadius;
+                        var height = nearestPointHeight / (distance * distance + 1);
+                        Heightmap[z, x] = (byte)height;
+                    }
+                }
+            }
         }
     }
 }
