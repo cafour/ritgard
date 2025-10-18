25eaa8b6adea991fecc9579f83344637c95d4e2c
Adam Štěpánek <git@cafour.cz>
2025-10-18 19:56:06 +0200

    Make job.sh finally work

--- data-processing/job.sh
+++ data-processing/job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #PBS -N ritgard
-#PBS -l select=1:ncpus=4:mem=40gb:scratch_local=40gb:ngpus=1:gpu_mem=40gb
+#PBS -l select=1:ncpus=4:mem=40gb:scratch_local=40gb:ngpus=1:gpu_mem=50gb
 #PBS -l walltime=1:00:00
 
 set -o errexit
@@ -14,7 +14,7 @@ cd $SCRATCHDIR/ritgard/data-processing
 mkdir -p datasets
 
 uv sync --preview-features extra-build-dependencies --no-build-isolation-package flash-attn
-uv run ./model-topics.py --llm --llm-model --embed-labels --embed-bodies --embed-comments --embed-model "Qwen/Qwen3-Embedding-8B" --flash-attention
+uv run ./model-topics.py --llm --embed-labels --embed-bodies --embed-comments --embed-model "Qwen/Qwen3-Embedding-8B" --flash-attention ./datasets/lume.json
 cp out/* /storage/brno2/home/xstepan1/out/
 
 rm -rf $SCRATCHDIR/*
