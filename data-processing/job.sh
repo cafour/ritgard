72fdc70870694d1b29cfb40ef550e81dd64cd3d1
Adam Štěpánek <git@cafour.cz>
2025-10-18 16:06:16 +0200

    Replace ritgard.def with job.sh

---
+++ data-processing/job.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#PBS -N ritgard
+#PBS -l select=1:ncpus=4:mem=40gb:scratch_local=40gb:ngpus=1:gpu_mem=40gb
+#PBS -l walltime=1:00:00
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+cd $SCRATCHDIR
+
+git clone git@gitlab.com:helveg/ritgard.git
+
+cd ritgard/data-processing
+cp /storage/brno2/home/xstepan1/ritgard/data-processing/.env .
+mkdir datasets
+cp /storage/brno2/home/xstepan1/ritgard/data-processing/datasets/lume.json ./datasets/
+
+uv sync
+uv run ./model-topics.py --embed-labels --embed-bodies --llm
+cp out/* /storage/brno2/home/xstepan1/out/
+
+rm -rf $SCRATCHDIR/*
